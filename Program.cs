ea12845d7bbfd9dff19fa670d8da90b3ae6f33ad
Matias <sagramtias04@gmail.com>
2024-05-15 23:17:54 -0300

    merge terminado

--- Program.cs
+++ Program.cs
@@ -6,6 +6,24 @@ b=a;
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
 
+Console.WriteLine("Ingrese un numero: ");
+string texto = Console.ReadLine();
+int num;
+
+
+bool resultado = int.TryParse(texto, out num);
+
+if (resultado){
+    int aux=0;
+    int dig=0;
+    while (num > 0)
+    {
+        aux = aux*10;
+        dig=num%10;
+        aux = aux + dig;
+        num=num/10;
+    }
+    Console.WriteLine($"El numero invertido es:  {aux}");
 
 
 
@@ -71,7 +89,7 @@ if (resultado){
     Console.WriteLine($"El numero invertido es:  {aux}");
 }
 
-//PUNTO 3
+//CalculadoraV2
 string numero;
 Console.WriteLine("Ingrese un numero: ");
 numero = Console.ReadLine();
